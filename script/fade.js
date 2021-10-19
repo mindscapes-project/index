1ab7e0e620968aa554a8d82ac701110a0914300d
mindscapes-project <mindscapes.kcl@gmail.com>
2021-10-19 14:56:07 +0100

    Add image placeholders

--- script/fade.js
+++ script/fade.js
@@ -6,7 +6,7 @@ $(window).scroll(function() {
         var objectStart = $(this).offset().top + 100; // $(this).outerHeight();
 
         if (objectStart < windowBottom) { //object comes into view (scrolling down)
-            if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+            if ($(this).css("opacity")==0) {$(this).fadeTo(800,1);}
         } else { //object goes out of view (scrolling up)
             if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
         }
