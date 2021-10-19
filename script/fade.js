36497faf85411a1f9260247de89ebfc296efcdaa
mindscapes-project <mindscapes.kcl@gmail.com>
2021-10-19 12:06:41 +0100

    Add first draft of content

--- script/fade.js
+++ script/fade.js
@@ -3,9 +3,9 @@ $(window).scroll(function() {
     var windowBottom = $(this).scrollTop() + $(this).innerHeight();
 
     $(".fade").each(function() {
-        var objectBottom = $(this).offset().top + $(this).outerHeight();
+        var objectStart = $(this).offset().top + 100; // $(this).outerHeight();
 
-        if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+        if (objectStart < windowBottom) { //object comes into view (scrolling down)
             if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
         } else { //object goes out of view (scrolling up)
             if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
